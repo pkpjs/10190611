9d7d5c1f113b721190148239385fdf0c9912b22f
pkpjs <144653284+pkpjs@users.noreply.github.com>
2023-10-12 15:07:03 +0900

    kdd.py
    ui 추가

--- kdd.py
+++ kdd.py
@@ -1,23 +1,35 @@
-import tkinter as tk
-from tkinter import filedialog
+import sys
 import pandas as pd
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextBrowser, QFileDialog
 
-def load_file():
-    file_path = filedialog.askopenfilename(title="데이터 파일 선택")
-    if file_path:
-        dataset = pd.read_csv(file_path)
-        text.insert('end', "파일을 성공적으로 불러왔습니다.\n")
-        text.insert('end', str(dataset.head()) + "\n")  # dataset의 첫 몇 행을 출력
-    else:
-        text.insert('end', "파일 선택이 취소되었거나 잘못된 파일을 선택하셨습니다.\n")
+class DataLoadingApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-root = tk.Tk()
-root.geometry("600x400")
+        self.setWindowTitle("파일 로더")
+        self.setGeometry(100, 100, 600, 400)
 
-load_button = tk.Button(root, text="데이터 파일 불러오기", command=load_file)
-load_button.pack()
+        self.load_button = QPushButton("데이터 파일 불러오기", self)
+        self.load_button.clicked.connect(self.load_file)
+        self.load_button.setGeometry(50, 50, 200, 30)
 
-text = tk.Text(root)
-text.pack()
+        self.text = QTextBrowser(self)
+        self.text.setGeometry(50, 100, 500, 250)
 
-root.mainloop()
+    def load_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "데이터 파일 선택")
+        if file_path:
+            try:
+                dataset = pd.read_csv(file_path)
+                self.text.append("파일을 성공적으로 불러왔습니다.")
+                self.text.append(str(dataset.head()))
+            except Exception as e:
+                self.text.append(f"파일 불러오기에 실패했습니다: {str(e)}")
+        else:
+            self.text.append("파일 선택이 취소되었거나 잘못된 파일을 선택하셨습니다.")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = DataLoadingApp()
+    window.show()
+    sys.exit(app.exec_())
